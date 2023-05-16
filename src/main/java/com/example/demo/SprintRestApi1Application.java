4ad01969578027031fa7f20feb7214e45523dd2a
Parthu-P <parthasaradhi1506@gmail.com>
2023-05-16 22:49:55 +0530

    spring projects

---
+++ src/main/java/com/example/demo/SprintRestApi1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintRestApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintRestApi1Application.class, args);
+	}
+
+}
